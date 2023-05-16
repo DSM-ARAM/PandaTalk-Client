d38a310ef7aa7b879cbb0fa857dc636d3a85f35e
Jin hyeon <kangjin627@gmail.com>
2023-05-16 20:15:39 +0900

    [ADD] 패키지 설정

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "../../components/button";
+
+export const MainPage = () =>{
+    return(
+        <div>
+            <Button Width="large">hihasfafai</Button>
+        </div>
+    );
+}
