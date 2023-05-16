5d8d934411aa75ebecab353a46f184f808bbc752
Jin hyeon <kangjin627@gmail.com>
2023-05-16 20:10:45 +0900

    [FIX] 파일 이름 변경

---
+++ src/styles/theme/color.ts
@@ -0,0 +1,9 @@
+export const Color = {
+  white: "#FFFFFF",
+  black: "000000",
+  main: "#AAEB94",
+  gray5: "#F2F2F2",
+  gray25: "#BFBFBF",
+  gray50: "#808080",
+  gray75: "#404040",
+};
--- styles/theme/index.ts
+++
@@ -1,9 +0,0 @@
-export const color = {
-  white: "#FFFFFF",
-  black: "000000",
-  main: "AAEB94",
-  gray5: "F2F2F2",
-  gray25: "BFBFBF",
-  gray50: "808080",
-  gray75: "404040",
-};
