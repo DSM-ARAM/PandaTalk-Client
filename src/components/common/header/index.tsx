914a1d80780909fa02f140e8412c291c812875af
Jin hyeon <kangjin627@gmail.com>
2023-07-07 20:21:27 +0900

    [ADD] 헤더 api연동

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,15 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import * as _ from "./style"
 
+const baseUrl = process.env.REACT_APP_BASEURL
+
 export const Header = () => {
+
+    const AccessToken = localStorage.getItem("AccessToken");
+    const RefreshToken = localStorage.getItem("RefreshToken");
+
+    const [headerData, setHeaderData] = useState();
+
+    useEffect(()=>{
+        axios({
+            method: "get",
+            url: `${baseUrl}/main/header`,
+            headers:{
+                "Authorization": `Bearer ${AccessToken}`
+            }
+        }).then((result)=>{
+            console.log(result);
+            setHeaderData(result.data.data);
+        })
+    },[])
+
     return(
         <>
             <_.HeaderBar>
                 <_.Flex>
                     <_.MainLogo src="/assets/img/Logo.svg"/>
                     <div>
-                        <_.TheacherInfo>산학협력부</_.TheacherInfo>
-                        <_.TheacherInfo>홍길동</_.TheacherInfo>
+                        <_.TheacherInfo>{headerData.userName}</_.TheacherInfo>
+                        <_.TheacherInfo>{headerData.userDepartment}</_.TheacherInfo>
                     </div>
                 </_.Flex>
             </_.HeaderBar>
